3813928e6ecf9dedd2c2dba4cd6afdf601636540
Syed Nazmus Sakib <nazmussak111@gmail.com>
2026-08-18 00:31:12 +0600

    first commit

---
+++ code/Demo.java
@@ -0,0 +1,13 @@
+public class Demo{
+    public static void main(String[] args) {
+        // byte b = 24;
+        // int i;
+        // i = b;
+        // System.out.println(i);
+
+        // int i = 300;
+        // byte b = i;
+        // System.out.println(b); // not possible
+        
+    }
+}
