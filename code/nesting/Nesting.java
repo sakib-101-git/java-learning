c7ee0970d49ebc54c8996a7a0c3dca408ac0bc41
Syed Nazmus Sakib <nazmussak.work@gmail.com>
2026-08-25 00:56:31 +0600

    nest

---
+++ code/nesting/Nesting.java
@@ -0,0 +1,20 @@
+class A { // outer class A
+    class B { // inner class B
+
+    }
+}
+
+/*
+
+    4 types of nested class
+    -> Static nested
+    -> inner class
+    -> local class
+    -> anonymous class
+
+
+    Why need nested class?
+    1. Logical Grouping
+    2. Better access to outer class
+
+*/
