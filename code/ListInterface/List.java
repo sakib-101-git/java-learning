b7392597fbec5f63155fc41c483e3edbfd579e13
Syed Nazmus Sakib <nazmussak.work@gmail.com>
2026-09-10 23:27:09 +0600

    List Interface Update

--- code/ListInterface/List.java
+++ code/ListInterface/List.java
@@ -14,12 +14,34 @@
         -> addLast()
         -> removeFirst()
         -> removeLast()
-        
+
 List Properties
     -> Elements have a position
     -> Duplicates are allowed
     -> Insertion order is preserved
     -> You can access elements by Index
+
+    E get(index)
+    set(index, E value)
+    example: set(1,6) // replace and put value 6 in 1st position
+
+    add(1, 6) // right shift the previous values and insert 6 in the existing list
+
+    addAll(index, Collection <? extends E>)
+
+    remove(index);
+
+    indexOf(Object o); // first index of a multiple occuring value
+
+    lastIndexOf(Object o); // last index of a multiple occuring value
+
+    listIterator() 
+        -> forward iteration
+        -> backward iteration
+        -> insert
+        -> remove
+
+    
 
 
 */
