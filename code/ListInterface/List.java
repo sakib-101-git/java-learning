e6a534b44439d9173b6dc39957569317ed6db615
Syed Nazmus Sakib <nazmussak.work@gmail.com>
2026-09-10 16:40:12 +0600

    Sequence Collection

---
+++ code/ListInterface/List.java
@@ -0,0 +1,25 @@
+/*
+-> Iterable
+    -> Collection
+        -> List
+            -> ArrayList
+            -> LinkedList
+            -> Vector
+                -> Stack
+
+    -> Sequence Collection -> List
+        -> getFirst()
+        -> getLast()
+        -> addFirst() 
+        -> addLast()
+        -> removeFirst()
+        -> removeLast()
+        
+List Properties
+    -> Elements have a position
+    -> Duplicates are allowed
+    -> Insertion order is preserved
+    -> You can access elements by Index
+
+
+*/
